1152735e3640ad34f497b7a5bd5c6edc30d756a5
kvasquez <kvasquez@k-lagan.com>
2026-06-28 18:27:23 +0200

    file ci/cd

--- infra/acr.tf
+++ infra/acr.tf
@@ -11,3 +11,9 @@ resource "random_string" "unique" {
   special = false
   upper   = false
 }
+
+resource "azurerm_role_assignment" "aks_pull_from_acr" {
+  scope                = azurerm_container_registry.acr.id
+  role_definition_name = "AcrPull"
+  principal_id         = azurerm_kubernetes_cluster.aks.identity[0].principal_id
+}
--- infra/outputs.tf
+++ infra/outputs.tf
@@ -8,3 +8,8 @@ output "aks_cluster_name" {
   description = "The name of the AKS cluster."
   value       = azurerm_kubernetes_cluster.aks.name
 }
+
+output "acr_login_server" {
+  description = "The login server of the Azure Container Registry."
+  value       = azurerm_container_registry.acr.login_server
+}
